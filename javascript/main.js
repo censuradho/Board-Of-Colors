54d63ba69190f239f02e890a9139d7a1ca8148bd
censuradho <gustavoleiteoliveira800@gmail.com>
2019-05-17 18:21:58 -0300

    adicionei audio ao click e modifiquei o nome da pasta.

--- javascript/main.js
+++ javascript/main.js
@@ -4,4 +4,10 @@ function mudaCor(e) {
     console.log(rgb);
     e.target.style.backgroundColor = rgb
 }
+function clickSound() {
+    var sound = new Audio;
+    sound.src = 'audio-button.mp3';
+    sound.play();
+}
 btn.addEventListener('click', mudaCor)
+btn.addEventListener('click', clickSound)
